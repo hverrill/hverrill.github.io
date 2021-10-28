bcdbdbc0d2ff5470dc5ad6ed842e70b789951887
hverrill <52667642+hverrill@users.noreply.github.com>
2021-10-28 15:40:35 -0400

    maybe don't need this?

--- scripts/main.js
+++ scripts/main.js
@@ -3,28 +3,29 @@
  * Henry Verrill
  */
 
-// Installs globals onto window:
-  // * Buffer
-  // * require (monkey-patches if already defined)
-  // * process
-  // You can pass in an arbitrary object if you do not wish to pollute
-  // the global namespace.
-  BrowserFS.install(window);
-  // Configures BrowserFS to use the LocalStorage file system.
-  BrowserFS.configure({
-    fs: "LocalStorage"
-  }, function(e) {
-    if (e) {
-      // An error happened!
-      throw e;
-    }
-    // Otherwise, BrowserFS is ready-to-use!
-  });
+
 
 /** namespace. */
 var hv = hv || {};
 // var marked = require('marked');
 var md = require('markdown-it')();
+// Installs globals onto window:
+// * Buffer
+// * require (monkey-patches if already defined)
+// * process
+// You can pass in an arbitrary object if you do not wish to pollute
+// the global namespace.
+// BrowserFS.install(window);
+// // Configures BrowserFS to use the LocalStorage file system.
+// BrowserFS.configure({
+// fs: "LocalStorage"
+// }, function(e) {
+// if (e) {
+//     // An error happened!
+//     throw e;
+// }
+// // Otherwise, BrowserFS is ready-to-use!
+// });
 var fs = require('fs');
 
 

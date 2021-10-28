2176ea094ad40b923788f52e45ee52ee85306d92
hverrill <52667642+hverrill@users.noreply.github.com>
2021-10-28 15:44:51 -0400

    please?

--- scripts/main.js
+++ scripts/main.js
@@ -3,29 +3,31 @@
  * Henry Verrill
  */
 
-
-
-/** namespace. */
-var hv = hv || {};
-// var marked = require('marked');
-var md = require('markdown-it')();
 // Installs globals onto window:
 // * Buffer
 // * require (monkey-patches if already defined)
 // * process
 // You can pass in an arbitrary object if you do not wish to pollute
 // the global namespace.
-// BrowserFS.install(window);
-// // Configures BrowserFS to use the LocalStorage file system.
-// BrowserFS.configure({
-// fs: "LocalStorage"
-// }, function(e) {
-// if (e) {
-//     // An error happened!
-//     throw e;
-// }
-// // Otherwise, BrowserFS is ready-to-use!
-// });
+BrowserFS.install(window);
+// Configures BrowserFS to use the LocalStorage file system.
+BrowserFS.configure({
+fs: "LocalStorage"
+}, function(e) {
+if (e) {
+    // An error happened!
+    throw e;
+}
+// Otherwise, BrowserFS is ready-to-use!
+});
+
+
+/** namespace. */
+var hv = hv || {};
+
+// var marked = require('marked');
+var MarkdownIt = require('markdown-it'),
+    md = new MarkdownIt();
 var fs = require('fs');
 
 
